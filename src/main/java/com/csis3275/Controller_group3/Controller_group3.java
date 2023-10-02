83c784448f0b29b2acc182d01f2fbd406bd3c66b
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-02 09:14:58 -0700

    refactor: added my model attribute to the html

--- src/main/java/com/csis3275/Controller_group3/Controller_group3.java
+++ src/main/java/com/csis3275/Controller_group3/Controller_group3.java
@@ -1,11 +1,15 @@
 package com.csis3275.Controller_group3;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.csis3275.Model_group3.Carlos_group3;
 
 @Controller
 public class Controller_group3 {
+	
+	public Carlos_group3 c1;
 	
 	@GetMapping("/")
 	public String showMainPage(Model model) {
@@ -17,6 +21,9 @@ public class Controller_group3 {
 	
 	@GetMapping("/carlos")
 	public String showCarlosPage(Model model) {
+		
+		c1 = new Carlos_group3("Carlos", 20, "Comp Studies & Info Systems");
+		model.addAttribute("carlos", c1);
 		
 		return "carlos.html";
 	}
