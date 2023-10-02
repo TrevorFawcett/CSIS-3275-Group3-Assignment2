186e4a8dfb6c3a3fa1c718524f72289c78dce476
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-02 08:55:10 -0700

    refactor: added attributes, constructor, and getters/setters

--- src/main/java/com/csis3275/Model_group3/Carlos_group3.java
+++ src/main/java/com/csis3275/Model_group3/Carlos_group3.java
@@ -1,5 +1,35 @@
 package com.csis3275.Model_group3;
 
 public class Carlos_group3 {
+	
+	String name;
+	String about;
+	
+	
+	public Carlos_group3() {
+		//default constructor
+	}
+	
+	public Carlos_group3(String name, String about) {
+		super();
+		this.name = name;
+		this.about = about;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	
+	
 
 }
