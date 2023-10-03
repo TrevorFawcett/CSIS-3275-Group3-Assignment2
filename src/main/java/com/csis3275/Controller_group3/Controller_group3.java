0d5f28cbde096cbbd4f8cf2639e6799e844b8676
TrevorFawcett <49043544+TrevorFawcett@users.noreply.github.com>
2023-10-02 17:23:40 -0700

    Feat: created bio page for myself

--- src/main/java/com/csis3275/Controller_group3/Controller_group3.java
+++ src/main/java/com/csis3275/Controller_group3/Controller_group3.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.csis3275.model_group3.model_trevor_group3;
+
 @Controller
 public class Controller_group3 {
 	
@@ -13,5 +15,12 @@ public class Controller_group3 {
 		
 		return "Page.html";
 	}
+	
+	@GetMapping("/trevor")
+	 public String changePage(Model model) {
+		model.addAttribute("trevor", new model_trevor_group3());
+	
+		return "TrevorFawcett";
+	 }
 
 }
---
+++ src/main/java/com/csis3275/model_group3/model_trevor_group3.java
@@ -0,0 +1,11 @@
+package com.csis3275.model_group3;
+
+public class model_trevor_group3 {
+
+	public String studentName = "Trevor Fawcett";
+	public String imgLink = "/images/profilepicture.png";
+	
+	public String bioInfo = "Studied at Douglas college since Winter 2018. Currently enrolled in Comp Study & Information Systems diploma."
+			+ "Age 26. Have some experience programming in HTML, CSS, Javascript, PHP, Java, Android development.";
+	
+}
